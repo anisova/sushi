562fc752cc4869000510943b3c1195f1af143cac
natali <nv@inbox.ru>
2020-02-03 10:37:20 +0300

    hamburger

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 (function(){
-  
+  const ham=document.querySelector('.hamburger');
+  const ham_menu=document.querySelector('.ham-menu');
+  const body=document.querySelector('body');
+  function toggleMenu(){
+      ham.classList.toggle('active');
+      ham_menu.classList.toggle('is-active');    
+      body.classList.toggle('locked'); 
+  }
+  ham.addEventListener('click',function(e){
+  e.preventDefault();  
+  toggleMenu();
+  })  
+  ham_menu.addEventListener('click',function(e){  
+    let target=e.target;
+    if (target.className==='menu__link') {    
+      toggleMenu();
+    }       
+  })    
   })();
-
+  
 
